b8c517b0faeb64208ec7ad89f59178449fc61274
Celal Karakoc <c.karakoc@live.nl>
2024-10-22 20:08:13 +0200

    The start of an adventure! :pirate_flag:

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -11,6 +12,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    np.add(1, 2)
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
